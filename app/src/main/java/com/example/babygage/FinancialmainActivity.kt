72ac3e01e87c13e7279010426d9b707fbc44e01b
piyujin <yjyj020201@gmail.com>
2022-11-08 09:54:36 +0900

    change FinancialmainActivity and LoginActivity
    change kotlin file (add viewbinding)

--- app/src/main/java/com/example/babygage/FinancialmainActivity.kt
+++ app/src/main/java/com/example/babygage/FinancialmainActivity.kt
@@ -2,10 +2,17 @@ package com.example.babygage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.babygage.databinding.ActivityFinancialmainBinding
+import com.example.babygage.databinding.ActivityLoginBinding
 
 class FinancialmainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_financialmain)
+
+        var binding : ActivityFinancialmainBinding
+        binding = ActivityFinancialmainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
